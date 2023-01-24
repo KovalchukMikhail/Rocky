27463baf459ffa17e7a7708c7c8a66831fb29b0c
Kovalchuk_Mikhail <kovalchuckmikhail@yandex.ru>
2023-01-24 14:24:32 +0300

    Homework001 added

--- Rocky/Migrations/20230124110738_addAplicationTypeToDatabase.Designer.cs
+++ Rocky/Migrations/20230124110738_addAplicationTypeToDatabase.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Rocky.Data;
 
@@ -10,9 +11,11 @@ using Rocky.Data;
 namespace Rocky.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230124110738_addAplicationTypeToDatabase")]
+    partial class addAplicationTypeToDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
